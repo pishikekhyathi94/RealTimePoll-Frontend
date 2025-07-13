688a698b49f1682749a3870703bcda9279e71156
Sreekar <Sreekar.amaram@gmail.com>
2025-07-13 12:22:49 -0500

    login page frontend

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,5 +1,5 @@
 <script setup>
-import ocLogo from "/oc_logo.png";
+import ocLogo from "../../public/Live_Poll.jpg"
 import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
 import UserServices from "../services/UserServices";
@@ -7,7 +7,7 @@ import UserServices from "../services/UserServices";
 const router = useRouter();
 
 const user = ref(null);
-const title = ref("Recipes");
+const title = ref("LIVE POLL");
 const logoURL = ref("");
 
 onMounted(() => {
@@ -45,13 +45,7 @@ function logout() {
         {{ title }}
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn class="mx-2" :to="{ name: 'recipes' }"> Recipes </v-btn>
-      <v-btn v-if="user === null" class="mx-2" :to="{ name: 'login' }">
-        Login
-      </v-btn>
-      <v-btn v-if="user !== null" class="mx-2" :to="{ name: 'ingredients' }">
-        Ingredients
-      </v-btn>
+      
       <v-menu v-if="user !== null" min-width="200px" rounded>
         <template v-slot:activator="{ props }">
           <v-btn icon v-bind="props">
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,7 +8,7 @@ import { createVuetify } from "vuetify";
 const OCTheme = {
   dark: false,
   colors: {
-    primary: "#80162B",
+    primary: "#21409a",
     secondary: "#E1E1E1",
     accent: "#47121D",
     success: "#47121D",

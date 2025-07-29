db4fbacc66e5638011ff14d9bab1bec909c5a8ee
Paulwilliams818 <paulwilliams.d@eagles.oc.edu>
2025-07-29 00:17:22 -0500

    delete whole quiz

--- src/views/ClassDetail.vue
+++ src/views/ClassDetail.vue
@@ -3,10 +3,14 @@ import { onMounted, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import AddQuizDialog from "../components/AddQuizDialog.vue";
 import ClassServices from "../services/ClassServices";
+import DeleteConfirmationDialog from "../components/DeleteConfirmationDialog.vue";
 
 const route = useRoute();
 const router = useRouter();
 const user = ref(null);
+const userRole = ref(null);
+const isDeleteDialogOpen = ref(false);
+const selectedQuiz = ref(null);
 const snackbar = ref({
   value: false,
   color: "",
@@ -17,6 +21,7 @@ const classId = route.params.classId;
 
 onMounted(async () => {
   user.value = JSON.parse(localStorage.getItem("user"));
+  userRole.value = localStorage.getItem("userRole");
   if (user) {
     await fetchQuizzes();
   }
@@ -52,11 +57,10 @@ async function handleAddQuiz(newQuiz) {
         await fetchQuizzes();
         snackbar.value.value = true;
         snackbar.value.color = "green";
-        snackbar.value.text = `${res?.data?.name} added successfully!`;
+        snackbar.value.text = `quiz added successfully!`;
       }
     });
   } catch (error) {
-    console.error("Error adding quiz:", error);
     snackbar.value.value = true;
     snackbar.value.color = "error";
     snackbar.value.text = "Failed to add quiz.";
@@ -69,11 +73,33 @@ function closeSnackBar() {
 }
 
 function goBack() {
-  router.push({ name: "professor" });
+    router.push({ name: "professor" });
+  }
+
+function openDeleteDialog(quiz) {
+  selectedQuiz.value = quiz;
+  isDeleteDialogOpen.value = true;
 }
 
-function goToQuiz(quizId) {
-  router.push(`/professor/class/${classId}/quiz/${quizId}`);
+function cancelDelete() {
+  isDeleteDialogOpen.value = false;
+  selectedQuiz.value = null;
+}
+async function deleteQuiz(quizId) {
+  await ClassServices.deleteQuiz(quizId)
+    .then(async (response) => {
+      if (response.status === 200) {
+        snackbar.value.value = true;
+        snackbar.value.color = "green";
+        snackbar.value.text = "Quiz deleted successfully!";
+        await fetchQuizzes();
+      }
+    })
+    .catch((error) => {
+      snackbar.value.value = true;
+      snackbar.value.color = "error";
+      snackbar.value.text = "Failed to delete quiz.";
+    });
 }
 
 </script>
@@ -101,24 +127,43 @@ function goToQuiz(quizId) {
     </v-row>
 
     <v-list two-line v-if="quizzes.length > 0">
-      <v-list-item
-        v-for="quiz in quizzes"
-        :key="quiz.id"
-         @click="goToQuiz(quiz.id)"
-        class="hoverable"
-      >
+      <v-list-item v-for="quiz in quizzes" :key="quiz.id">
         <v-list-item-content>
-          <v-list-item-title>{{ quiz.name }}</v-list-item-title>
-          <v-list-item-subtitle class="mb-3">{{
-            quiz.description
-          }}</v-list-item-subtitle>
+          <div role="button" @click="goToQuiz(quiz.id)">
+            <v-list-item-title>{{ quiz.name }}</v-list-item-title>
+            <v-list-item-subtitle class="mb-3">{{
+              quiz.description
+            }}</v-list-item-subtitle>
+          </div>
+          <v-list-item-action class="mr-auto text-right justify-end">
+            <v-btn
+              @click="openDeleteDialog(quiz)"
+              class="my-2"
+              prepend-icon="mdi-trash-can"
+              color="primary"
+              v-if="userRole === 'professor'"
+              variant="flat"
+            >
+              Delete Quiz
+            </v-btn>
+          </v-list-item-action>
         </v-list-item-content>
       </v-list-item>
     </v-list>
     <div v-else class="text-center pa-4 text-grey">
       No quizzes available for this class.
     </div>
-    <AddQuizDialog v-model="showAddQuiz" @submit="handleAddQuiz" />
+
+    <AddQuizDialog
+      v-model="showAddQuiz"
+      @submit="handleAddQuiz"
+    />
+     <DeleteConfirmationDialog
+    v-model="isDeleteDialogOpen"
+    message="Are you sure you want to delete this Quiz?"
+    @confirm="deleteQuiz(selectedQuiz.id)"
+    @cancel="cancelDelete"
+  />
   </v-container>
   <v-snackbar v-model="snackbar.value" rounded="pill">
     {{ snackbar.text }}
